8d94acf3e1ef05217d27464e5de1f4cc70fba568
kb <kb@v01.io>
2026-02-01 10:41:43 +0100

    Release 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bonbonbon.MixProject do
   def project do
     [
       app: :bonbonbon,
-      version: "0.1.0",
+      version: "1.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
